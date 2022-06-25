45937781439daabd969ae5db236d33760b0579e7
pb325 <pb325@student.le.ac.uk>
2022-06-25 18:41:50 +0100

    Added header and brand component to the page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Footer, Blog, Possibility, Features, WhatModern, Header } from './containers';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import {CTA, Brand, Navbar } from './components';
 
 import './App.css';
@@ -13,7 +13,7 @@ const App = () => {
         <Header />
       </div>
       <Brand />
-      <WhatModern />
+      <WhatGPT3 />
       <Features />
       <Possibility />
       <CTA />
--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 import './brand.css';
+import {google, slack, atlassian, dropbox, shopify } from './imports';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className="gpt3__brand section__padding">
+      <div className=""><img src={google} alt="google logo" /></div>
+      <div className=""><img src={slack} alt="slack logo" /></div>
+      <div className=""><img src={atlassian} alt="atlassian logo" /></div>
+      <div className=""><img src={dropbox} alt="dropbox logo" /></div>
+      <div className=""><img src={shopify} alt="shopify logo" /></div>
+    </div>
   )
 }
 
